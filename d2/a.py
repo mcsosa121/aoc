c2a96db5f6562898f39acffb28f299d763374793
Mike Sosa <mcsosa121@gmail.com>
2021-12-01 21:23:08 -0800

    D2
    Day 2

---
+++ d2/a.py
@@ -0,0 +1,45 @@
+"""
+It seems like the submarine can take a series of commands like forward 1, down 2, or up 3:
+
+forward X increases the horizontal position by X units.
+down X increases the depth by X units.
+up X decreases the depth by X units.
+Note that since you're on a submarine, down and up affect your depth, and so they have the opposite result of what you might expect.
+
+The submarine seems to already have a planned course (your puzzle input). You should probably figure out where it's going. For example:
+
+forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2
+Your horizontal position and depth both start at 0. The steps above would then modify them as follows:
+
+forward 5 adds 5 to your horizontal position, a total of 5.
+down 5 adds 5 to your depth, resulting in a value of 5.
+forward 8 adds 8 to your horizontal position, a total of 13.
+up 3 decreases your depth by 3, resulting in a value of 2.
+down 8 adds 8 to your depth, resulting in a value of 10.
+forward 2 adds 2 to your horizontal position, a total of 15.
+After following these instructions, you would have a horizontal position of 15 and a depth of 10. (Multiplying these together produces 150.)
+
+Calculate the horizontal position and depth you would have after following the planned course. What do you get if you multiply your final horizontal position by your final depth?
+"""
+commands = []
+with open("a.txt", "r+") as file:
+    commands += [l.split(' ') for l in file.readlines()]
+
+pos = (0, 0)
+
+if __name__ == "__main__":
+    for cmd, val in commands:
+        if cmd == "forward":
+            pos = (pos[0] + int(val), pos[1])
+        elif cmd == "down":
+            pos = (pos[0], pos[1] + int(val))
+        elif cmd == "up":
+            pos = (pos[0], pos[1] - int(val))
+
+    print(f"Final Horizontal Pos: {pos[0]}, Final Depth: {pos[1]}")
+    print(f"Res: {pos[0] * pos[1]}")
